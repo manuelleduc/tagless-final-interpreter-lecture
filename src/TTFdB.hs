43a4cbe7300b03967a4be76f6849bafed6fecef2
Manuel Leduc <manuel.leduc@gmail.com>
2017-09-27 15:29:00 +0200

    windows modifcations

--- src/TTFdB.hs
+++ src/TTFdB.hs
@@ -9,6 +9,16 @@ class Symantics repr where
   lam :: repr (a, h) b -> repr h (a -> b)
   app :: repr h (a -> b) -> repr h a -> repr h b
 
+class MulSYM repr where
+  mult :: repr Int -> repr Int -> repr Int
+
+class BoolSYM repr where
+  bool :: Bool -> repr Bool
+  leq :: repr Int -> repr Int -> repr Bool
+  if_ :: repr Bool -> repr a -> repr a -> repr a
+
+class FixSYM repr where
+  fix :: (repr a -> repr a) -> repr a
 
 td1 :: Symantics repr => repr h Int
 td1 = add (int 1) (int 2)
@@ -23,6 +33,11 @@ td2o' = app td2o (int 3)
 --td3 :: Symantics repr => repr h ((Int -> Int) -> Int)
 td3 :: Symantics repr => repr h ((Int -> Int) -> Int)
 td3 = lam (add (app z (int 1)) (int 2))
+
+
+tpow = lam (\x -> fix (\self -> lam (\n ->
+                        if_ (leq n (int 0)) (int 1)
+                            (mul x (app self (add n (int (-1))))))))
 
 
 newtype R h a = R { unR :: h -> a }
